3cd5bfa258a85810c8ee9cabb60e0f1adf6884be
Andrew <plaschinskiyandriy@gmail.com>
2026-05-27 10:47:03 +0300

    Update task-5.js

--- js/task-5.js
+++ js/task-5.js
@@ -1,1 +1,14 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
 
+const changeColorBtn = document.querySelector(`.change-color`);
+const colorValue = document.querySelector(`.color`);
+
+changeColorBtn.addEventListener(`click`, () => {
+  const randomColor = getRandomHexColor();
+  document.body.style.backgroundColor = randomColor;
+  colorValue.textContent = randomColor;
+});
