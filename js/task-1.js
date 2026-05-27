7bb0f71a2c132794375f2a0436d847033cb9f524
Andrew <plaschinskiyandriy@gmail.com>
2026-05-27 10:46:21 +0300

    Update task-1.js

--- js/task-1.js
+++ js/task-1.js
@@ -1,1 +1,9 @@
-
+const categoriesElem = document.querySelector(`ul#categories`);
+const categoriesItems = categoriesElem.querySelectorAll(`li.item`);
+console.log(`Number of categories: ${categoriesItems.length}`);
+categoriesItems.forEach(item => {
+  const categoryTitle = `Category: ${item.querySelector(`h2`).textContent}`;
+  const categoryElements = item.querySelectorAll(`li`);
+  console.log(categoryTitle);
+  console.log(`Elements: ${categoryElements.length}`);
+});
