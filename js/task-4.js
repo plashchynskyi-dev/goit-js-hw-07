57bed6114b262eb2653c9b96031660ce5463b598
Andrew <plaschinskiyandriy@gmail.com>
2026-05-27 10:46:53 +0300

    Update task-4.js

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,25 @@
+const formElem = document.querySelector(`.login-form`);
+formElem.addEventListener(`submit`, onFormSubmit);
 
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  const emailValue = email.value.trim();
+  const passwordValue = password.value.trim();
+
+  if (emailValue === `` || passwordValue === ``) {
+    alert(`Please fill in all the fields!`);
+    return;
+  }
+  const formData = {
+    email: emailValue,
+    password: passwordValue,
+  };
+
+  console.log(formData);
+  event.currentTarget.reset();
+}
