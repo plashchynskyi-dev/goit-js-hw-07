f172b9079d960948609e66c35efd093836058c94
Andrew <plaschinskiyandriy@gmail.com>
2026-05-27 10:46:43 +0300

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -1,1 +1,7 @@
+const inputElem = document.querySelector(`input#name-input`);
+const spanElem = document.querySelector(`span#name-output`);
+inputElem.addEventListener(`input`, onInputChange);
 
+function onInputChange(event) {
+  spanElem.textContent = event.currentTarget.value.trim() || `Anonymous`;
+}
